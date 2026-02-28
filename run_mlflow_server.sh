2c653015a1b633c362da04268436a2906626103b
Mikhail <moskovchenkomike@gmail.com>
2026-02-28 21:09:51 +0000

    Add data check experiments

--- run_mlflow_server.sh
+++ run_mlflow_server.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+set -a
+
+source .env 
+
+set +a
+
 export MLFLOW_S3_ENDPOINT_URL=https://storage.yandexcloud.net
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
@@ -5,5 +13,5 @@ export AWS_BUCKET_NAME=$S3_BUCKET_NAME
 
 mlflow server \
   --backend-store-uri postgresql://$DB_DESTINATION_USER:$DB_DESTINATION_PASSWORD@$DB_DESTINATION_HOST:$DB_DESTINATION_PORT/$DB_DESTINATION_NAME\
-    --default-artifact-root s3://$AWS_BUCKET_NAME \
-    --no-serve-artifacts
+  --default-artifact-root s3://$AWS_BUCKET_NAME \
+  --no-serve-artifacts
